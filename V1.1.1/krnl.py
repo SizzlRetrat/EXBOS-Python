adfb98da92c710479ba09aeece16e5cbe677067e
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-05-30 11:50:27 +0100

    Create krnl.py

---
+++ V1.1.1/krnl.py
@@ -0,0 +1,4 @@
+EXBC = "(c) copyright EXB hbLTD"
+EXBMSG = "welcome to exbos python v1.1.1 aka exbos 23 edition, exbos is a free open source python os but main source code is (c) exbos"
+print(EXBC + EXBMSG) *1
+open(V1.1.1/ipc.py)
